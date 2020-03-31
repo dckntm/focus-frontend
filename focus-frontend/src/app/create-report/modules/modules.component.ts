a3a77db41de853900124dfb98f07e0b21d2665e5
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-01 00:07:08 +0300

    get value from button in modules list

--- focus-frontend/src/app/create-report/modules/modules.component.ts
+++ focus-frontend/src/app/create-report/modules/modules.component.ts
@@ -1,31 +1,27 @@
-import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, OnInit, AfterViewInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { ModuleComponent } from '../module/module.component';
-import { Routes, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-modules',
   templateUrl: './modules.component.html',
   styleUrls: ['./modules.component.scss']
 })
-export class ModulesComponent implements OnInit {
+export class ModulesComponent implements AfterViewInit {
   
   state = {
     modules: [],
-    currentModule: 1,
+    currentModule: '',
   }
   
   realSizeModules = 0;
 
   @ViewChild('model', {read: ViewContainerRef}) private model: any;
-  ngOnInit(){
-    
+  ngAfterViewInit(){
+    this.addNewModule();
   }
-
-
-
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -35,10 +31,6 @@ export class ModulesComponent implements OnInit {
 
   constructor(private breakpointObserver: BreakpointObserver, private componentFactoryResolver: ComponentFactoryResolver,) {}
 
-  setTitle(){
-
-  }
-
   addNewModule(){
     this.realSizeModules += 1;
     this.state.modules.push({value: 'Module' + this.realSizeModules})
@@ -46,11 +38,17 @@ export class ModulesComponent implements OnInit {
     let moduleComponent = this.componentFactoryResolver.resolveComponentFactory( ModuleComponent );
     let moduleComponentRef = this.model.createComponent( moduleComponent );
     (<ModuleComponent>moduleComponentRef.instance).value = {
-      title: 'module'+this.realSizeModules,
+      title: 'Module'+this.realSizeModules,
     };
-    console.log(this.realSizeModules)
-    console.log('Module added')
+    this.state.currentModule = 'Module'+this.realSizeModules;
+    console.log(this.state.modules);
+    console.log(this.state.currentModule);
+    console.log('Module added');
   }
 
+  setModule(value){
+    this.state.currentModule = value;
+    console.log("current module is "+this.state.currentModule)
+  }
   
 }
