52d13464237773f9ace2aa842429e54a6997e817
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-02 15:16:58 +0300

    add true url and add HttpModule to AppModule

--- focus-frontend/src/app/app.module.ts
+++ focus-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,6 +33,7 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    HttpClientModule,
     
   ],
   exports: [
