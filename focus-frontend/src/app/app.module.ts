5bdd4446edcc1e03bafe4359c01562f927712b82
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-07 12:01:48 +0300

    add create-timetable component

--- focus-frontend/src/app/app.module.ts
+++ focus-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { CreateTimetableComponent } from './create-timetable/create-timetable/create-timetable.component';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { MatListModule } from '@angular/material/list';
     AppComponent,
     ModuleComponent,
     ModulesComponent,
+    CreateTimetableComponent,
   ],
   imports: [
     BrowserModule,
---
+++ focus-frontend/src/app/create-timetable/create-timetable/create-timetable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-timetable',
+  templateUrl: './create-timetable.component.html',
+  styleUrls: ['./create-timetable.component.scss']
+})
+export class CreateTimetableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
