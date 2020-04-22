eb7784bad97fd7e51a1c7a17bab82f82c8de04da
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-22 12:08:02 +0300

    add routing & create new component & add bootstrap

--- focus-frontend/src/app/app-routing.module.ts
+++ focus-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModulesComponent } from '../app/create-report/modules/modules.component';
 import { ModuleComponent } from '../app/create-report/module/module.component';
 import { CreateTimetableComponent } from '../app/create-timetable/create-timetable/create-timetable.component'
-
+import { AuthComponent } from '../app/auth/auth.component'
 
 const routes: Routes = [
   {path: '', component: ModulesComponent},
-  {path: 'timetable', component: CreateTimetableComponent}
+  {path: 'timetable', component: CreateTimetableComponent},
+  {path: 'auth', component: AuthComponent}
 ];
 
 @NgModule({
