5a6403aab7d881d8fc751c56ef48288c4d0b8d31
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-22 13:31:48 +0300

    connect html form with ts form & add reactiveForms to ngModule

--- focus-frontend/src/app/app.module.ts
+++ focus-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -38,7 +38,7 @@ import { AuthComponent } from './auth/auth.component';
     MatIconModule,
     MatListModule,
     HttpClientModule,
-    
+    ReactiveFormsModule,
   ],
   exports: [
     MatSidenavModule,
