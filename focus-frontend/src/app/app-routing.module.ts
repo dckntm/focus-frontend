2ee73ec905c59a10fabd47367b69801a59cf8250
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-25 11:02:59 +0300

    add routing and add components to page component

--- focus-frontend/src/app/app-routing.module.ts
+++ focus-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModulesComponent } from '../app/create-report/modules/modules.component';
 import { ModuleComponent } from '../app/create-report/module/module.component';
 import { CreateTimetableComponent } from '../app/create-timetable/create-timetable/create-timetable.component'
-import { AuthComponent } from '../app/auth/auth.component'
+import { AuthComponent } from '../app/auth/auth.component';
+import { OrganisationsListPageComponent } from '../app/organizations-list/organisations-list-page/organisations-list-page.component'
 
 const routes: Routes = [
   {path: '', component: ModulesComponent},
   {path: 'timetable', component: CreateTimetableComponent},
-  {path: 'auth', component: AuthComponent}
+  {path: 'auth', component: AuthComponent},
+  {path: 'organizations', component: OrganisationsListPageComponent}
 ];
 
 @NgModule({
