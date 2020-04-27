da936aa79b116dd89db7c7117aa6a2e8cc913130
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-27 18:13:50 +0300

    add routing & 2 components to main

--- focus-frontend/src/app/app-routing.module.ts
+++ focus-frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { ModulesComponent } from '../app/create-report/modules/modules.component
 import { ModuleComponent } from '../app/create-report/module/module.component';
 import { CreateTimetableComponent } from '../app/create-timetable/create-timetable/create-timetable.component'
 import { AuthComponent } from '../app/auth/auth.component'
+import { TimetableListPageComponent } from './timetable-list/timetable-list-page/timetable-list-page.component';
 
 const routes: Routes = [
   {path: '', component: ModulesComponent},
   {path: 'timetable', component: CreateTimetableComponent},
-  {path: 'auth', component: AuthComponent}
+  {path: 'auth', component: AuthComponent},
+  {path: 'timetable-list', component: TimetableListPageComponent}
 ];
 
 @NgModule({
