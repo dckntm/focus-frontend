87cf87e6b2b0f5a48a3017b9563e61108d0652f3
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-28 15:40:42 +0300

    add post method in service for create organisation

--- focus-frontend/src/app/servises/create-org.service.ts
+++ focus-frontend/src/app/servises/create-org.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateOrgService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json; charset=utf-8"
+    })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postOrg(title: string, adress: string, number: number){
+    return this.http
+    .post<string>("http://localhost5000/api/new/org",{
+      title,
+      adress,
+      number
+    })
+    .pipe(catchError(this.errorHandler))
+  }
+
+  errorHandler(error) {
+    let errorMessage = "You are dumbhead and have error";
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
