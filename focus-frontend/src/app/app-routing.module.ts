d7e6207993ef104834814402e6216951e5e9b30a
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-28 15:00:32 +0300

    change routing

--- focus-frontend/src/app/app-routing.module.ts
+++ focus-frontend/src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ import { CreateTimetableComponent } from '../app/create-timetable/create-timetab
 import { AuthComponent } from '../app/auth/auth.component'
 import { TimetableListPageComponent } from './timetable-list/timetable-list-page/timetable-list-page.component';
 import { OrganisationsListPageComponent } from '../app/organizations-list/organisations-list-page/organisations-list-page.component'
+import { CreateUserComponent } from './create-user/create-user.component';
+import { CreateOrgComponent } from './create-org/create-org.component';
 
 const routes: Routes = [
   {path: '', component: ModulesComponent},
   {path: 'timetable', component: CreateTimetableComponent},
   {path: 'auth', component: AuthComponent},
   {path: 'timetable-list', component: TimetableListPageComponent},
-  {path: 'organizations', component: OrganisationsListPageComponent}
+  {path: 'organizations', component: OrganisationsListPageComponent},
+  {path: 'c-u', component: CreateUserComponent},
+  {path: 'c-o', component: CreateOrgComponent},
 ];
 
 @NgModule({
