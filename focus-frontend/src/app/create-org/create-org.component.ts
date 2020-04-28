1dedc754c0e49e0d58587f8e9db109194c7bf42f
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-28 16:10:09 +0300

    add post method to org component

--- focus-frontend/src/app/create-org/create-org.component.ts
+++ focus-frontend/src/app/create-org/create-org.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CreateOrgService } from '../servises/create-org.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-org',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-org.component.scss']
 })
 export class CreateOrgComponent implements OnInit {
+  createForm: FormGroup;
+  error: "";
+  actionType: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private pageService: CreateOrgService) { }
 
   ngOnInit(): void {
+    this.createForm = this.formBuilder.group({
+      title: ["", Validators.required],
+      adress: ["", Validators.required],
+      number: ["", Validators.required]
+    });
+  }
+
+  get form(){
+    return this.createForm.controls;
+  }
+
+  onSubmit() {
+    if (this.createForm.invalid) {
+      return;
+    }
+
+    console.log(this.form.title, this.form.adress, this.form.number);
+
+    this.pageService
+      .postOrg(this.form.title.value, this.form.adress.value, this.form.number.value)
+      .pipe(first())
   }
 
 }
