37f528a9f79c9802c9451fe55e245513b53fae0f
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-28 15:31:58 +0300

    add post method to user service

--- focus-frontend/src/app/servises/create-user.service.ts
+++ focus-frontend/src/app/servises/create-user.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateUserService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json; charset=utf-8"
+    })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postUser(id: string, name: string, username:string, password: string){
+    return this.http
+    .post<string>("http://localhost5000/api/new/user",{
+      id,
+      name,
+      username,
+      password
+    })
+    .pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error) {
+    let errorMessage = "You are dumbhead and have error";
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
