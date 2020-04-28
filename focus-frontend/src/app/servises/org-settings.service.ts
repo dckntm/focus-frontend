eee1fc677428716ec9b3e579bed75ecb3cea4b0c
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-29 00:27:47 +0300

    add get methods to service

--- focus-frontend/src/app/servises/org-settings.service.ts
+++ focus-frontend/src/app/servises/org-settings.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Organization } from '../models/organization';
+import { retry, catchError } from 'rxjs/operators';
+import { SimpleUser } from '../models/simple-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrgSettingsService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json; charset=utf-8"
+    })
+  };
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getInfo(): Observable<Organization>{
+    return this.http
+    .get<Organization>("http//localhost5000/api/organization/info", this.httpOptions)
+    .pipe(retry(1), catchError(this.errorHandler));
+  }
+
+  getUsers(): Observable<SimpleUser[]>{
+    return this.http
+    .get<SimpleUser[]>("http//localhost5000/api/organization/users", this.httpOptions)
+    .pipe(retry(1), catchError(this.errorHandler));
+  }
+
+  errorHandler(error) {
+    let errorMessage = "You are dumbhead and have error";
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
