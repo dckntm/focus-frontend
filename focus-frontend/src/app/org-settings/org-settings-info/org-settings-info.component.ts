3e2eed0b0b67e4943215864908433f8f7261ba8e
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-29 19:44:04 +0300

    add post method in component

--- focus-frontend/src/app/org-settings/org-settings-info/org-settings-info.component.ts
+++ focus-frontend/src/app/org-settings/org-settings-info/org-settings-info.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Organization } from 'src/app/models/organization';
 import { Observable } from 'rxjs';
 import { OrgSettingsService } from 'src/app/servises/org-settings.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-org-settings-info',
@@ -10,18 +12,43 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./org-settings-info.component.scss']
 })
 export class OrgSettingsInfoComponent implements OnInit {
-  Organization$: Observable<Organization>
+  Organization$: Observable<Organization>;
+  infoForm: FormGroup;
+  error: "";
+  actionType: string;
 
-  constructor(private pageService: OrgSettingsService, private route: ActivatedRoute) { }
+  constructor(private pageService: OrgSettingsService, private route: ActivatedRoute, private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.loadInfo();
+
+    this.infoForm = this.formBuilder.group({
+      title: ["", Validators.required],
+      adress: ["", Validators.required],
+      number: ["", Validators.required]
+    });
 
     this.Organization$.subscribe(x => console.log(x));
   }
 
   loadInfo(){
     this.Organization$ = this.pageService.getInfo();
+  }
+
+  get form(){
+    return this.infoForm.controls;
+  }
+
+  onSubmit() {
+    if (this.infoForm.invalid) {
+      return;
+    }
+
+    console.log(this.form.title, this.form.adress, this.form.number);
+
+    this.pageService
+      .saveInfo(this.form.title.value, this.form.adress.value, this.form.number.value)
+      .pipe(first())
   }
 
 }
