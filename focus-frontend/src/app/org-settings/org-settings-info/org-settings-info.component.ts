3a02fc5295c6c9c05f1b8ed75f832a779a08944a
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-04-29 19:31:35 +0300

    add get methods to component

--- focus-frontend/src/app/org-settings/org-settings-info/org-settings-info.component.ts
+++ focus-frontend/src/app/org-settings/org-settings-info/org-settings-info.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Organization } from 'src/app/models/organization';
+import { Observable } from 'rxjs';
+import { OrgSettingsService } from 'src/app/servises/org-settings.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-org-settings-info',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./org-settings-info.component.scss']
 })
 export class OrgSettingsInfoComponent implements OnInit {
+  Organization$: Observable<Organization>
 
-  constructor() { }
+  constructor(private pageService: OrgSettingsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadInfo();
+
+    this.Organization$.subscribe(x => console.log(x));
+  }
+
+  loadInfo(){
+    this.Organization$ = this.pageService.getInfo();
   }
 
 }
--- focus-frontend/src/app/org-settings/org-settings-users/org-settings-users.component.ts
+++ focus-frontend/src/app/org-settings/org-settings-users/org-settings-users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SimpleUser } from 'src/app/models/simple-user';
+import { Observable } from 'rxjs';
+import { OrgSettingsService } from 'src/app/servises/org-settings.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-org-settings-users',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./org-settings-users.component.scss']
 })
 export class OrgSettingsUsersComponent implements OnInit {
+  users$: Observable<SimpleUser[]>;
 
-  constructor() { }
+  constructor(private pageService: OrgSettingsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadUsers();
+
+    this.users$.subscribe(x => console.log(x))
+  }
+
+  loadUsers(){
+    this.users$ = this.pageService.getUsers();
   }
 
 }
