1b0959cd2e6dfe22517769cd6edac12144fb2770
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-05-10 13:47:01 +0300

    change model and component work

--- focus-frontend/src/app/models/report.ts
+++ focus-frontend/src/app/models/report.ts
@@ -1,3 +1,23 @@
 export class Report{
     id: string;
+    title: string;
+    reportTemplateId: string;
+    assignedOrganizationId: string;
+    status: number;
+    deadline: Date;
+    questionnaireAnswers: [{
+        title: string;
+        order: number;
+        sectionAnswers: [{
+            title: string;
+            order: number;
+            questionAnswers: [{
+                title: string;
+                order: number;
+                answer: string;
+                answerType: number;
+            }]
+        }]
+    }] ;
+    tableAnswers: []
 }
--- focus-frontend/src/app/report/report-page/report-page.component.ts
+++ focus-frontend/src/app/report/report-page/report-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Report } from 'src/app/models/report';
 import { ReportService } from 'src/app/servises/report.service';
+import { ReportModuleComponent } from '../report-module/report-module.component';
 
 @Component({
   selector: 'app-report-page',
@@ -11,8 +12,11 @@ import { ReportService } from 'src/app/servises/report.service';
   styleUrls: ['./report-page.component.scss']
 })
 export class ReportPageComponent implements OnInit {
+  dataR: Report;
   report$: Observable<Report>;
   reportId: string;
+
+  @ViewChild('model', {read: ViewContainerRef}) private model: any;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -20,10 +24,23 @@ export class ReportPageComponent implements OnInit {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private pageService: ReportService) {}
+  constructor(private breakpointObserver: BreakpointObserver, private pageService: ReportService, private componentFactoryResolver: ComponentFactoryResolver) {}
 
   ngOnInit(){
     this.report$ = this.pageService.getReport(this.reportId);
+    this.transformData();
+    for(let i = 0; i < this.dataR.questionnaireAnswers.length; i++){
+
+    }
+  }
+
+  createModules(){
+    let moduleComponent = this.componentFactoryResolver.resolveComponentFactory( ReportModuleComponent );
+    let moduleComponentRef = this.model.createComponent( moduleComponent );
+  }
+
+  transformData(){
+    this.report$.subscribe((data:Report)=>this.dataR = data);
   }
 
 }
