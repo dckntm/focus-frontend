ed671012a9293b308182596156794dd2988fcbca
SixDevelop <47978391+SixDevelop@users.noreply.github.com>
2020-05-10 12:30:30 +0300

    add get method to service and page

--- focus-frontend/src/app/report/report-page/report-page.component.ts
+++ focus-frontend/src/app/report/report-page/report-page.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Report } from 'src/app/models/report';
+import { ReportService } from 'src/app/servises/report.service';
 
 @Component({
   selector: 'app-report-page',
   templateUrl: './report-page.component.html',
   styleUrls: ['./report-page.component.scss']
 })
-export class ReportPageComponent {
+export class ReportPageComponent implements OnInit {
+  report$: Observable<Report>;
+  reportId: string;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +20,10 @@ export class ReportPageComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private pageService: ReportService) {}
+
+  ngOnInit(){
+    this.report$ = this.pageService.getReport(this.reportId);
+  }
 
 }
--- focus-frontend/src/app/servises/report.service.ts
+++ focus-frontend/src/app/servises/report.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Report } from '../models/report';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json; charset=utf-8"
+    })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getReport(id: string): Observable<Report> {
+    return this.http
+      .get<Report>("http://localhost:5000/api", this.httpOptions)
+      .pipe(retry(1), catchError(this.errorHandler));
+  }
+
+  errorHandler(error) {
+    let errorMessage = "You are dumbhead and have error";
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+  
 }
